377ba23889cc2780248cdf5b497b913c1c6a96cb
rojoetech <v.andrianantenaina@etechconsulting-mg.com>
2023-04-18 15:22:15 +0300

    [DEL]: delete tv website rar [ADD]: gvp trip module base

---
+++ gpv_trip/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2023 eTech SA
+# License AGPL-3.0
+
+{
+    "name": "GPV - Trip",
+    "description": """
+        Odoo crm application for GPV""",
+    "author": "eTech",
+    "website": "https://www.etechconsulting-mg.com",
+    "category": "",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+        "crm",
+    ],
+    "data": [
+
+    ],
+    'assets': {
+        },
+    "application": True,
+}
