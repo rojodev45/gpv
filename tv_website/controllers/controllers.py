1e8fe2997638c3526b5c7f01c2d13ac763f1c3c3
rojoetech <v.andrianantenaina@etechconsulting-mg.com>
2023-04-20 15:08:11 +0300

    [ADD]: dynamization message us footer (gpv_footer)

--- tv_website/controllers/controllers.py
+++ tv_website/controllers/controllers.py
@@ -2,6 +2,8 @@
 # License AGPL-3.0
 
 from odoo import http
+import json
+
 
 class Website(http.Controller):
     @http.route('/devis', auth='user', website="True")
@@ -13,3 +15,4 @@ class Website(http.Controller):
     def terre_voyage_template(self, **kw):
         values = {}
         return http.request.render("tv_website.terre_voyage_template", values)
+
