24be431afd27ae542fd3ad50ef0b220fd2c17900
a.andriamandimbinian <a.andriamandimbinian@etechconsulting-mg.com>
2023-04-25 16:40:01 +0300

    [FIX] xml template

--- gpv_theme_customize/static/src/js/terre_voyage.js
+++ gpv_theme_customize/static/src/js/terre_voyage.js
@@ -8,5 +8,37 @@ odoo.define('jam_portal.profile_controller', function(require) {
             nav: true,
             items: 3,
         });
+        $(function() {
+            $('.nav-item.dropdown:first-child').hover(function() {
+                $('#top_menu_collapse').css({
+                    backgroundColor: 'rgb(26 45 62 / 75%)',
+                    width: '90%'
+                });
+            }, function() {
+                $('#top_menu_collapse').css({
+                    backgroundColor: '',
+                    width: 'fit-content'
+                });
+            });
+        });
+        $(".read-more").click(function(){
+            var readMoreText = "...Plus";
+            var readLessText = "Afficher moins";
+            var $shortElem = $(this).parent().find(".text.short");
+            var $fullElem = $(this).parent().find(".text.full");
+
+            if($shortElem.is(":visible"))
+            {
+                $shortElem.hide();
+                $fullElem.show();
+                $(this).text(readLessText);
+            }
+            else
+            {
+                $shortElem.show();
+                $fullElem.hide();
+                $(this).text(readMoreText);
+            }
+        });
     });
 });
